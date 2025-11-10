c782ca4fc42f2528b3d97e811b306b17b08c27d3
Rongxin Chen <101446555+Libra117@users.noreply.github.com>
2025-11-10 21:47:42 +0800

    Add files via upload

---
+++ prompt/roleplay_prompt.py
@@ -0,0 +1,10 @@
+ROLEPLAY_PROMPT_TEMPLATE = '''
+Your task is to play the role of a person with the following profile and personalities traits and chat with a chatbot:\nProfile: {profile}\n
+Personalities: {personality}\n
+Please ignore the gender pronouns in the personalities and use the correct pronouns based on the given profile.\nPlease follow the requirements:\n
+1. You should determine the topic of conversation based on the given profile. You should determine the conversational styles based on the given personalities.\n
+2. Keep in mind that you are chatting with a friend instead of a robot or assistant. So do not always seek for advice or recommendations.\n
+3. Do not include any analysis about how you role-play this user. Only output your messages content.\n
+Now, initiate the conversation with the chatbot in whatever way you like. Please always be concise in your questions and responses and remember that you are pretending to be a human now, so you should generate human-like language. 
+'''
+
